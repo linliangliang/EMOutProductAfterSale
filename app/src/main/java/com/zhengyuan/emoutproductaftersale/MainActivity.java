b2d2291a15184d4f4b5597abba69673b3a3a8fe7
linliangliang <349698837@qq.com>
2018-10-09 15:48:54 +0800

    D:/Git/

--- app/src/main/java/com/zhengyuan/emoutproductaftersale/MainActivity.java
+++ app/src/main/java/com/zhengyuan/emoutproductaftersale/MainActivity.java
@@ -2,28 +2,41 @@ package com.zhengyuan.emoutproductaftersale;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 public class MainActivity extends Activity {
-    String sname= null;
+    String sname = null;
     //sname=EMProApplicationDelegate.userInfo.getUserId();
-   private ImageButton backBtn;
+    private ImageButton backBtn;
     private ImageButton menu;
-    private Button Outer=null;
-    private Button AfterSale=null;
+    private Button Outer = null;
+    private Button AfterSale = null;
+
+    private ImageView testtakephoto = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.title_tv);
-        if(sname !=null){
-            textView.setText("驻外售后平台"+"-"+sname);
-        }else{
+        if (sname != null) {
+            textView.setText("驻外售后平台" + "-" + sname);
+        } else {
             textView.setText("驻外售后平台");
         }
 
@@ -35,25 +48,61 @@ public class MainActivity extends Activity {
                 finish();
             }
         });
-        menu=(ImageButton)findViewById(R.id.main_menu_bn);
+        menu = (ImageButton) findViewById(R.id.main_menu_bn);
         menu.setVisibility(View.GONE);
 
-        Outer=(Button)findViewById(R.id.outer);
-        AfterSale=(Button)findViewById(R.id.aftersale);
-        Outer.setOnClickListener(new View.OnClickListener(){
+        Outer = (Button) findViewById(R.id.outer);
+        AfterSale = (Button) findViewById(R.id.aftersale);
+        Outer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,OuterActivity.class);
+                Intent intent = new Intent(MainActivity.this, OuterActivity.class);
                 startActivity(intent);
             }
 
         });
-        AfterSale.setOnClickListener(new View.OnClickListener(){
+        AfterSale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,AfterSaleActivity.class);
+                Intent intent = new Intent(MainActivity.this, AfterSaleActivity.class);
                 startActivity(intent);
             }
         });
+
+
+        testtakephoto = (ImageView) findViewById(R.id.testtakephoto);
+        testtakephoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, ImageToGallery.filePath);
+                startActivityForResult(intent, 2);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            //Toast.makeText(TakePhotoAndSave.this,"RESULT_OK",Toast.LENGTH_SHORT).show();
+            //拍照结果处理
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                Bitmap bm = (Bitmap) bundle.get("data");
+                if (bm != null) {
+                    ImageToGallery.saveImageToGallery(getApplicationContext(), bm);
+
+                    Glide.with(this).load(ImageToGallery.filePath).asBitmap().into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+                            //图片加载完成
+                            testtakephoto.setImageBitmap(bitmap);
+                        }
+                    });
+
+                }
+            } else {
+                //Toast.makeText(Carout_details.this, "没有压缩的图片数据", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
